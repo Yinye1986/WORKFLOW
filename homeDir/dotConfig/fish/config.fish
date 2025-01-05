4124f55ee07061303251bd2a671d1bbe6a09562a
Chris Hung <yinye1986@gmail.com>
2025-01-05 16:58:59 +0800

    bak

--- homeDir/dotConfig/fish/config.fish
+++ homeDir/dotConfig/fish/config.fish
@@ -5,8 +5,8 @@ end
 # My alias
 alias :q="exit"
 
-alias ll="ls -l"
-alias la="ls -la"
+alias ll="ls -lh"
+alias la="ls -lah"
 alias lld="lsblk"
 alias llu="lsusb"
 
@@ -20,14 +20,20 @@ alias xie='sudo st-flash write build/main.bin 0x8000000'
 alias cl='make clean'
 alias mk='make'
 alias tiao='sudo openocd -f stlink.cfg'
-
 alias done='mk & ca & xie'
-alias hexo='npx hexo'
 
 # My Apps
-alias draw.io='/home/chris/myApps/draw.io/drawio'
 alias d='go run main.go --alac-max 192000 --select'
 
+function pi
+    sudo pacman -S $argv
+end
+function ps
+    sudo pacman -Ss $argv
+end
+function pu
+    sudo pacman -Rns $argv
+end
 function fm
 	set tmp (mktemp -t "yazi-cwd.XXXXXX")
 	yazi $argv --cwd-file="$tmp"
